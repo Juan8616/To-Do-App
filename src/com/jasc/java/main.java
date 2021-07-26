ffbe66872621a91eaffb4539764fafb633380de1
rambo <rambo@192.168.0.11>
2021-07-26 11:37:30 -0500

    Initial Commit

---
+++ src/com/jasc/java/main.java
@@ -0,0 +1,16 @@
+package com.jasc.java;
+
+/**
+ *
+ * @author rambo
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
